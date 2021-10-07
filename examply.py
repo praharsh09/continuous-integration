5b87046aa420aa45bd02b874df67a783e6fe9f08
praharsh09 <40351276+praharsh09@users.noreply.github.com>
2021-10-07 12:22:43 +0200

    addition and subtraction function

--- examply.py
+++ examply.py
@@ -4,3 +4,20 @@ def print_temperature():
     print(temperature)
 def test_print_temperature():
     assert 1+2 == 3
+    
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+
+def subtract(a, b):
+    return a + b  # <--- fix this 
+
+
+# uncomment the following test
+#def test_subtract():
+#    assert subtract(2, 3) == -1
